c84ea7495fce494ab064d33533f29012a60a3a91
MacbookPro <panluobu@gmail.com>
2019-09-23 16:55:48 +0800

    1

--- demo/die/die.py
+++ demo/die/die.py
@@ -3,7 +3,7 @@ from random import randint
 class Die():
     """表示一个骰子的类"""
 
-    def __init__(self, num_sides=6)
+    def __init__(self, num_sides=6):
         """骰子默认为六面"""
         self.num_sides = num_sides
 
---
+++ demo/die/die_visual test.py
@@ -0,0 +1,31 @@
+from die import Die
+import pygal
+
+#创建一个六面骰子
+die_1 = Die(8)
+die_2 = Die(8)
+
+#掷几次骰子，并将结果保存在一个列表中
+results = []
+for roll_num in range(10000000):
+    result = die_1.roll() + die_2.roll()
+    results.append(result)
+
+#分析结果:计算出现频率
+frequencies = []
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2, max_result+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+hist = pygal.Bar()
+
+hist.title = "results of rolling two D8 10000000 times."
+hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16']
+hist._x_title = "Results"
+hist._y_title = "Frequency of Result"
+
+hist.add('D8+D8', frequencies)
+hist.render_to_file('die_visual.svg')
+#print(results)
+print(frequencies)
